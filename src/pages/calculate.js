6f495659921b4a1adcc460691b337a92e28553e9
Leonardo Louie Ordonez <leonardolouie30@gmail.com>
2021-04-11 11:00:54 +0800

    fix hero name

---
+++ src/pages/calculate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "components/footer";
+import Navbar from "components/navbar";
+
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
