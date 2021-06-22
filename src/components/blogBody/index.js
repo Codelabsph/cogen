37c9cae5c9709a480475a5f0b7ce5141ce015b40
Leonardo Louie Ordonez <leonardolouie30@gmail.com>
2021-06-23 02:30:48 +0800

    added markedown parser on FE

--- src/components/blogBody/index.js
+++ src/components/blogBody/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import FbIcon2 from "public/assets/fb-icon2.svg";
 import BlogSubHeading from "src/components/blogSubHeading";
-import marked from "marked";
 import { formatDate, fbShareLink } from "src/helpers/utils.service";
+import ReactMarkdown from "react-markdown";
 
 const BlogBody = ({ ...props }) => {
   return (
@@ -12,7 +12,7 @@ const BlogBody = ({ ...props }) => {
           date={formatDate(props?.published_at)}
           author={props?.author?.name}
         />
-        {props?.content && marked(props?.content)}
+        {props?.content && <ReactMarkdown children={props?.content} />}
         <div className="w-full flex mx-auto justify-center space-x-4">
           <div className="flex space-x-4 items-center">
             <FbIcon2 />
